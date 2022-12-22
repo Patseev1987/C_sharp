a16a07f4690cb935d10b884ba8fe55e2eb267823
Bogdan Patseev <patseev1987@gmail.com>
2022-12-22 19:42:11 +0300

    add exercise10

---
+++ exercise10/Program.cs
@@ -0,0 +1,7 @@
+﻿    Console.WriteLine("We have a random number");
+int a = new Random().Next(100,1000);    
+Console.WriteLine(a);    
+Console.WriteLine("***The result***");
+int b = (a/10)%10;
+Console.WriteLine(b);
+  
