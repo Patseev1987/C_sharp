b50e41545c3a28d7c6d2ce7878d0b0e5a25a6d27
Bogdan Patseev <patseev1987@gmail.com>
2022-12-22 19:26:59 +0300

    add exercise15

---
+++ exercise15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine ("Enter the day of the week number ");
+    int numberDayOfWeek=Convert.ToInt32(Console.ReadLine());
+if (numberDayOfWeek==6 || numberDayOfWeek==7)
+{
+  Console.WriteLine("This day is day off!");
+}
+if (numberDayOfWeek>0 && numberDayOfWeek<6)
+ {
+      Console.WriteLine("This day is not day off!");
+}
+if (numberDayOfWeek<=0 || numberDayOfWeek>=8)
+{
+    Console.WriteLine("You made a mistake!");
+    Console.WriteLine("Try again and enter a nuber from 1 to 7");
+}
