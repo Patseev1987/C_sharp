7f1b63e90166895d79e3a3c4db061b2809a6d3a4
Bogdan Patseev <patseev1987@gmail.com>
2023-01-20 18:38:50 +0300

    added exercise 41, 43

---
+++ exercise41/Program.cs
@@ -0,0 +1,20 @@
+﻿
+void PositiveCounter(int Length)
+{
+    int[] MyArray = new int[Length];
+    int Count = 0;
+    for (int i = 0; i < Length; i++)
+    {
+        Console.WriteLine($"Enter {i + 1} number:");
+        int x = int.Parse(Console.ReadLine());
+        MyArray[i] = x;
+        if (x > 0)
+            Count++;
+    }
+    Console.WriteLine($"You entered {Count} positive numbers.");
+}
+Console.Clear();
+Console.WriteLine("How many numbers do you want to enter?");
+int Size = int.Parse(Console.ReadLine());
+Console.WriteLine();
+PositiveCounter(Size);
---
+++ exercise43/Program.cs
@@ -0,0 +1,27 @@
+﻿void crossPoint(double k1, int b1, double k2, int b2)
+{
+    if (k1 == k2 && b1 == b2)
+        Console.WriteLine("Эти прямые совпадают!");
+    else if (k1 == k2)
+        Console.WriteLine("Эти прямые парралельны!");
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        Console.WriteLine($"Точка пересечения прямых: ({x},{y}).");
+    }
+}
+Console.Clear();
+Console.WriteLine("Введите k1 для первой прямой y=k1*x+b1.");
+double kk1 = double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Введите b1 для первой прямой y=k1*x+b1.");
+int bb1 = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Введите k2 для второй прямой y=k2*x+b2.");
+double kk2 = double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Введите b2 для второй прямой y=k2*x+b2.");
+int bb2 = int.Parse(Console.ReadLine());
+Console.WriteLine();
+crossPoint(kk1, bb1, kk2, bb2);
